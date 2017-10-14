4aa43dde7b2caa44eeeefd2b0b98e2e4a8bf5385
sap15e <anjalibrown12@gmail.com>
2017-10-14 16:37:32 -0400

    Merge branch 'master' of C:\Users\sap15e\Desktop\potter-pals with conflicts.

--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
@@ -1,7 +1,17 @@
 package edu.fsu.cs.cen4020.potterpals;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
