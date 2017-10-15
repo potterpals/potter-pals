2b05957830b7aaad663b244a70e7fd08800a5925
sap15e <anjalibrown12@gmail.com>
2017-10-15 15:03:27 -0400

    Fixed the error that made the app not run by fixing the activity_main.xml file

--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
