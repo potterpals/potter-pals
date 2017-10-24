c104a4eaace868a85cae4eaa21c357179b548655
sap15e <anjalibrown12@gmail.com>
2017-10-24 10:45:21 -0400

    Added the functionality where when user clicks on sign up on main activity, it lets them fill things out.

--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/MainActivity.java
@@ -1,11 +1,13 @@
 package edu.fsu.cs.cen4020.potterpals;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.TextView;
@@ -20,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button signUp = (Button) findViewById(R.id.login);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterUser.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
---
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/RegisterUser.java
@@ -0,0 +1,26 @@
+package edu.fsu.cs.cen4020.potterpals;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by sap15e on 10/24/2017.
+ */
+
+public class RegisterUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+
+
+
+
+
+    }
+}
