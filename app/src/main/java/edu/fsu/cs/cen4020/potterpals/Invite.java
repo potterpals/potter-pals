19fd032ef30fb47aa64e13e923b0451e7d74f28b
sap15e <anjalibrown12@gmail.com>
2017-11-03 10:58:43 -0400

    Modified register.xml

--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/Invite.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/Invite.java
@@ -1,6 +1,11 @@
 package edu.fsu.cs.cen4020.potterpals;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 /**
  * Created by sap15e on 11/2/2017.
@@ -8,4 +13,17 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Invite extends AppCompatActivity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+
 }
