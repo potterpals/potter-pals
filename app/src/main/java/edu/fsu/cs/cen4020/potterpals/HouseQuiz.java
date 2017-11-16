b36cf64cc461d844fd5848a5ab021c9edf73248e
vidhigandhi <vidhi.1996.gandhi@gmail.com>
2017-11-15 21:52:08 -0500

    Implemented quiz page navigation logic in HouseQuiz.java

--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/HouseQuiz.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/HouseQuiz.java
@@ -1,10 +1,12 @@
 package edu.fsu.cs.cen4020.potterpals;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 /**
@@ -13,11 +15,13 @@ import android.widget.RadioButton;
 
 public class HouseQuiz extends AppCompatActivity
 {
-    Button next;
+    Button next, end;
+    TextView question;
     int GryffCount=0;
     int HuffCount=0;
     int SlythCount=0;
     int RavenCount=0;
+    int questionCount = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +30,21 @@ public class HouseQuiz extends AppCompatActivity
         setContentView(R.layout.quiz);
 
         next = (Button) findViewById(R.id.Next);
+        end = (Button) findViewById(R.id.results);
+        question = (TextView) findViewById(R.id.question_text);
         final RadioButton sel1 = (RadioButton)findViewById(R.id.sel1);
         final RadioButton sel2 = (RadioButton)findViewById(R.id.sel2);
         final RadioButton sel3 = (RadioButton)findViewById(R.id.sel3);
         final RadioButton sel4 = (RadioButton)findViewById(R.id.sel4);
 
+        //initially all button = invisible
         next.setVisibility(View.INVISIBLE);
+        end.setVisibility(View.INVISIBLE);
+
+
+        //Question assigned to the textview
+        question.setText("Question " + questionCount);
+
 
         sel1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                                             @Override
@@ -79,5 +92,33 @@ public class HouseQuiz extends AppCompatActivity
                                             }
                                         }
         );
+
+        /*
+            Implementing questions : Flow of events
+                If questions are less than 10 then next button should take user to the next question
+                If all 10 questions are displayed -> "get results" button becomes visible
+                "Get results" takes user to view house they belong to
+                */
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++questionCount;
+                if (questionCount <= 10) {
+                    //If question limit is not reached; display next question
+
+                    question.setText("Question " + questionCount);
+                }
+                else if (questionCount > 10){
+                    //"get results button becomes visible"
+                    end.setVisibility(View.VISIBLE);
+                    //"Next" button is invisible to any clicks - can go no further
+                    next.setVisibility(View.INVISIBLE);
+                }
+            }//end view
+        }//end listener
+        );
+
     }
+
+
 }
--- app/src/main/java/edu/fsu/cs/cen4020/potterpals/LoginUser.java
+++ app/src/main/java/edu/fsu/cs/cen4020/potterpals/LoginUser.java
@@ -1,11 +1,8 @@
 package edu.fsu.cs.cen4020.potterpals;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
 
 /**
  * Created by sap15e on 11/1/2017.
@@ -19,11 +16,5 @@ public class LoginUser extends AppCompatActivity
         setContentView(R.layout.login);
 
         Button login = (Button) findViewById(R.id.button_login);
-        
-
-
-
-
-
     }
 }
